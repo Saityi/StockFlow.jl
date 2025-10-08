3cd0f041bb5c676b24fd7146f917429d3a777ede
Alex Alegre <6503910+Saityi@users.noreply.github.com>
2025-10-08 09:03:05 -0600

    remove old stockflow code; deprecate older examples

--- validate-notebook-examples.sh
+++ validate-notebook-examples.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 mkdir -p jlexamples
-./ipynb-to-jl.sh "full_fledged_schema_examples"
+# ./ipynb-to-jl.sh "full_fledged_schema_examples"
 ./ipynb-to-jl.sh "full_fledged_schema_examples_new"
 rm -f jlexamples/full_fledged_schema_examples*checkpoint*
 julia -p auto --project="." -e 'include("./run_notebooks.jl")'
