94ade551daf53a0c991ba8aa0f9ececd3f81648f
Alex Alegre <6503910+Saityi@users.noreply.github.com>
2025-10-10 11:05:57 -0600

    Fix removing too many characters in ipynb->jl step

--- ipynb-to-jl.sh
+++ ipynb-to-jl.sh
@@ -2,7 +2,7 @@
 
 find ./examples -type f -name "*.ipynb" | while read f; do
     fname=$(basename "$f")
-    dname=$(dirname "$f" | cut -c13-) # Get directory name; remove ./examples/
+    dname=$(dirname "$f" | cut -c12-) # Get directory name; remove ./examples/
      mkdir -p "./jlexamples/$dname"
      jq -j '.cells
             | map( select(.cell_type == "code") | .source + ["\n\n"] )
